2365444959ede95cdc461f8b9a7b08a64815442a
Toma <inverkip644@gmail.com>
2025-12-22 09:01:59 +0000

    Create ArmaOBS.bat
    Change the paths located inside of the <>.

---
+++ ArmaOBS.bat
@@ -0,0 +1,6 @@
+@echo off
+start "" "steam://run/1874880"
+
+cd /d "<Path to obs-studio/bin/64bit>"
+
+start "" "<Path to obs-studio/bin/64bit/obs64.exe" --startreplaybuffer --disable-shutdown-check
